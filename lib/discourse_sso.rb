efc986347e73d7be126b6822767a749db9bdcc32
andrea longhi <andrea@spaghetticode.it>
2014-02-10 12:07:22 +0100

    bugfix

--- lib/discourse_sso.rb
+++ lib/discourse_sso.rb
@@ -9,11 +9,10 @@ module DiscourseSso
     def ensure_sso_login
       if cookies[:discourse_sso_cookie].present?
         cookie_user_id = cookies[:discourse_sso_cookie] # makes sense to have this value encrypted in the cookie
-        unless current_user && current_user.id == cookie_user_id
+        unless current_user && current_user.id == cookie_user_id.to_i
           user = User.find cookie_user_id
           log_on_user(user)
-          # TODO this doesn't look to be necessary
-          # redirect_to url_for # we need to "reload" the page, so the user sees that he's logged in
+          redirect_to url_for # we need to "reload" the page, so the user sees that he's logged in
         end
       else
         reset_session
